30810eee700cc29a0267c87349eb9c17ff334ff8
wfwf1990 <576589099@qq.com>
2018-06-28 02:22:58 +0800

    day02

--- Python基础/3丶循环语句/while循环/打印99乘法表-while嵌套循环.py
+++ Python基础/3丶循环语句/while循环/打印99乘法表-while嵌套循环.py
@@ -4,7 +4,7 @@ col = 1
 while col <= 9:
     line = 1
     while col >= line:
-        print("%d*%d=%d " %(line,col,line*col),end="" )
+        print("%d*%d=%d\t" %(line,col,line*col),end="" )
         line += 1
     print("")
     col += 1
